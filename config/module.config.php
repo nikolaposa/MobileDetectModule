723867dc7a0fa4aa94c196e8a2481cc89f64f108
Jonathan Maron <jonathan.maron@theimagingsource.com>
2017-06-15 08:27:55 +0200

    Code quality fix

--- config/module.config.php
+++ config/module.config.php
@@ -22,7 +22,6 @@ return [
         'factories' => [
             'mobileDetect' => MobileDetectControllerFactory::class,
         ],
-
     ],
     'view_helpers'       => [
         'factories' => [
